e52674f7a652ab30988ec6f6bbab2e713db124cb
craz1973 <aninavarathana@gmail.com>
2026-02-01 21:10:52 +0530

    added tailwind CSS

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import SubmitButton from './components/Button/SubmitButton'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar />
      <Footer/>
+     <SubmitButton />
     </>
   )
 }
