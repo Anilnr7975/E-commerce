42a0ba84376c5d52cf396ceac0cf1485e2f6c830
craz1973 <aninavarathana@gmail.com>
2026-02-01 20:52:10 +0530

    resolved import issue

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 
 
 function App() {
